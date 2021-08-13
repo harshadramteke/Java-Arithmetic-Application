702b3a5a0848fb89f7eea31d934196a3f5ad1396
Harshad Ramteke <harshadramteke358@gmail.com>
2021-08-13 10:14:21 +0530

    Initial Commit

---
+++ ArithMeticApplicationConept/src/com/allstate/arithmetic/ARithmeticCalculation.java
@@ -0,0 +1,18 @@
+package com.allstate.arithmetic;
+
+public class ARithmeticCalculation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		  int num1= 10, num2=30, result=0;
+		  
+		  result= num1+ num2;
+		  System.out.println("\n Addition of Two Number" +result);
+		  
+		  
+		  
+	}
+
+}
